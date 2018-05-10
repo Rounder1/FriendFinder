738ec4d1a340db1db7124396ddd2e72429f5b30c
Rounder1 <tolerdavid@gmail.com>
2018-05-09 20:02:58 -0400

    fixed json added model

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,8 @@ var friendData = require("../data/friends.js");
 module.exports = function (app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(friendData);
+        console.log('friends api end point');
+        res.send(JSON.stringify(friendData));
     });
 
     app.post("/api/friends", function(req, res) {
