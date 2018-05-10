c204a77af2a6518da907046ae05f9843d802a389
Rounder1 <tolerdavid@gmail.com>
2018-05-09 21:27:11 -0400

    top tier array parsing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,41 @@ var friendData = require("../data/friends.js");
 
 module.exports = function (app) {
 
+
+
+
+
     app.get("/api/friends", function(req, res) {
-        console.log('friends api end point');
         res.send(JSON.stringify(friendData));
     });
 
+
     app.post("/api/friends", function(req, res) {
+        
+        var userScores = req.body.scores;
+        var parsedUserScore = [];
+        var friendScoresArray = [];
+        var friendScoresParsed = [];
+
+
+        for (i = 0; i < friendData.length; i++) {
+            friendScoresArray.push(friendData[i].scores);
+        }
+
+
+        for (i = 0; i < friendScoresArray.length; i++) {
+            friendScoresParsed.push(friendScoresArray[i].map(num => parseInt(num, 10)));
+        }
+
+        console.log(friendScoresParsed);
+
+
+        for (i = 0; i < userScores.length; i++) {
+            parsedUserScore.push(parseInt(userScores[i]));
+        }
+        
+        
         friendData.push(req.body);
+    
     });
 }
